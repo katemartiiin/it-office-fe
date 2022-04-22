d5f16b20f8995b54edb47c257965b06e0940d9cc
Jerick-CM <jmangaluz@gmail.com>
2022-04-22 12:39:31 +0800

    add test datatables

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-      { src: './plugins/echo', mode: 'client' }
+      { src: './plugins/echo', mode: 'client' },
+      { src: '~/plugins/vue-good-table', ssr: false }
   ],
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/users/logins2.vue
+++ pages/users/logins2.vue
@@ -13,52 +13,37 @@
         </div>
         <div class="block w-full overflow-x-auto">
           <!-- Requests table -->
-          <table
-            id="requestsTable"
-            class="dt-table items-center w-full bg-transparent border-collapse"
+          <vue-good-table
+            :pagination-options="{
+              enabled: true,
+            }"
+            :columns="columns"
+            :rows="rows"
+            :line-numbers="true"
           >
-            <thead>
-              <tr>
-                <th
-                  v-for="(header, index) in headers"
-                  :key="index"
-                  class="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left text-gray-700"
+            <template slot="table-row" slot-scope="props">
+              <span v-if="props.column.field == 'action'">
+                <button
+                  :key="props.row.id"
+                  v-if="props.row.approval == 0"
+                  :class="props.row.classname"
+                  class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  aria-expanded="false"
+                  @click="approve(props.row)"
                 >
-                  {{ header.name }}
-                </th>
-              </tr>
-            </thead>
-            <tbody class="text-gray-600 px-5">
-              <tr v-for="item in tabledata" :key="item.id">
-                <td>{{ item.no }}</td>
-                <td>{{ item.id }}</td>
-                <td>{{ item.name }}</td>
-                <td>{{ item.email }}</td>
-                <td>{{ item.status }}</td>
-                <td>
-                  <button
-                    :key="item.id"
-                    v-if="item.approval == 0"
-                    :class="item.classname"
-                    class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    aria-expanded="false"
-                    @click="approve(item)"
-                  >
-                    Approve
-                  </button>
-
-                  <button
-                    v-else
-                    :class="item.classname"
-                    class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    aria-expanded="false"
-                  >
-                    Approve
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                  Approve
+                </button>
+                <button
+                  v-else
+                  :class="props.row.classname"
+                  class="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  aria-expanded="false"
+                >
+                  Approve
+                </button>
+              </span>
+            </template>
+          </vue-good-table>
         </div>
       </div>
     </div>
@@ -82,6 +67,49 @@ export default {
       ],
       error: '',
       tabledata: [],
+
+      tabledata: [],
+      columns: [
+        {
+          label: '#',
+          field: 'no',
+        },
+        {
+          label: 'Request No',
+          field: 'id',
+        },
+        {
+          label: 'Name',
+          field: 'name',
+        },
+
+        {
+          label: 'Email',
+          field: 'email',
+        },
+        {
+          label: 'Browser',
+          field: 'browser',
+        },
+        {
+          label: 'Status',
+          field: 'status',
+        },
+        {
+          label: 'Action',
+          field: 'action',
+        },
+      ],
+      rows: [
+        { id: 1, name: 'John', age: 20, createdAt: '', score: 0.03343 },
+        {
+          id: 2,
+          name: 'Jane',
+          age: 24,
+          createdAt: '2011-10-31',
+          score: 0.03343,
+        },
+      ],
     }
   },
   created() {
@@ -92,34 +120,70 @@ export default {
   },
   methods: {
     async approve(item) {
-      
+        console.log(item.no)
       this.currentIndex = item
-
       console.log(item)
-      this.editedIndex = this.tabledata.indexOf(item)
+      this.editedIndex = this.rows.indexOf(item.no)
       console.log('index is')
       console.log(this.editedIndex)
 
-      let payload = new FormData()
-      let table_id = this.tabledata[this.editedIndex].id
-      payload.append('id', table_id)
+      // let payload = new FormData()
+      // let table_id = this.tabledata[this.editedIndex].id
+      // payload.append('id', table_id)
 
-      try {
-        this.$axios
-          .$post(`api/userlogin/approve/${table_id}`, payload, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          })
-          .then((res) => {
-            this.tabledata[this.tabledata.indexOf(item)].status = 'Approved'
-            this.tabledata[this.tabledata.indexOf(item)].classname =
-              'bg-gray-500'
-          })
-          .catch((error) => {})
-          .finally(() => {})
-      } catch (error) {}
+      // try {
+      //   this.$axios
+      //     .$post(`api/userlogin/approve/${table_id}`, payload, {
+      //       headers: {
+      //         'Content-Type': 'multipart/form-data',
+      //       },
+      //     })
+      //     .then((res) => {
+      //       this.tabledata[this.tabledata.indexOf(item)].status = 'Approved'
+      //       this.tabledata[this.tabledata.indexOf(item)].classname =
+      //         'bg-gray-500'
+      //     })
+      //     .catch((error) => {})
+      //     .finally(() => {})
+      // } catch (error) {}
     },
+    // async fetch() {
+    //   await this.$axios
+    //     .post('/api/fetch/requests')
+    //     .then((response) => {
+    //       this.requests = response.data.requests
+
+    //       if (this.dataTable) {
+    //         this.dataTable.destroy()
+    //       }
+
+    //       var data = []
+    //       var rowcount = 1
+    //       this.$nextTick(() => {
+    //         for (const i in this.requests) {
+    //           data.push({
+    //             no: rowcount,
+    //             id: this.requests[i].id,
+    //             name: this.requests[i].user.fullname,
+    //             email: this.requests[i].user.email,
+    //             approval: this.requests[i].is_approved,
+    //             status:
+    //               this.requests[i].is_approved == 1 ? 'Approved' : 'Pending',
+    //             classname:
+    //               this.requests[i].is_approved == 1
+    //                 ? 'bg-gray-500'
+    //                 : 'bg-blue-500',
+    //           })
+    //           rowcount++
+    //         }
+
+    //         this.tabledata = data
+    //       })
+    //     })
+    //     .catch((error) => {
+    //       this.error = error
+    //     })
+    // },
     async fetch() {
       await this.$axios
         .post('/api/fetch/requests')
@@ -133,7 +197,6 @@ export default {
           var data = []
           var rowcount = 1
           this.$nextTick(() => {
-
             for (const i in this.requests) {
               data.push({
                 no: rowcount,
@@ -141,6 +204,7 @@ export default {
                 name: this.requests[i].user.fullname,
                 email: this.requests[i].user.email,
                 approval: this.requests[i].is_approved,
+                browser: this.requests[i].browser,
                 status:
                   this.requests[i].is_approved == 1 ? 'Approved' : 'Pending',
                 classname:
@@ -151,27 +215,13 @@ export default {
               rowcount++
             }
 
-            this.tabledata = data
+            this.rows = data
+            console.log(this.rows)
           })
         })
         .catch((error) => {
           this.error = error
         })
-    },
-    renderprinbtn(id) {
-      return (
-        '<button class="approve-btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" aria-expanded="false"  req-id="' +
-        id +
-        '" > Approve<\/button>'
-      )
-    },
-    kate() {
-      var table_id
-      $('.approve-btn').click(function () {
-        console.log($(this).attr('req-id'))
-        table_id = $(this).attr('req-id')
-        this.approve(table_id)
-      })
     },
   },
 }
