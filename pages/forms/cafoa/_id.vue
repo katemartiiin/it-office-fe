8bbe61c3e8d248e06d32d738f6a976ae4a2f4f67
Kate Martin <katejaneen23@gmail.com>
2022-05-20 10:11:23 +0800

    Modify file list

--- pages/forms/cafoa/_id.vue
+++ pages/forms/cafoa/_id.vue
@@ -174,11 +174,11 @@
                             <p class="text-xs">No supporting files uploaded.</p>
                         </div>
                         <!-- has files > loop <li> -->
-                        <div class="border-1 rounded py-2 px-5 my-2">
+                        <div class="border-1 rounded px-5 py-2 my-2">
                             <ul>
-                                <li class="my-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 1</a></li>
-                                <li class="my-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 2</a></li>
-                                <li class="my-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 3</a></li>
+                                <li class="my-2 hover:bg-gray-200 px-5 py-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 1</a></li>
+                                <li class="my-2 hover:bg-gray-200 px-5 py-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 2</a></li>
+                                <li class="my-2 hover:bg-gray-200 px-5 py-2"><a href="#" class="text-xs"><i class="fas fa-file mr-2"></i>File 3</a></li>
                             </ul>
                         </div>
                     </div>
