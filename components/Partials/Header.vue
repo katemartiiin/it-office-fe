539a16418b033bb4d1f919fb1f4c496593900d40
Kate Martin <katejaneen23@gmail.com>
2022-05-31 09:41:45 +0800

    Layout modification

--- components/Partials/Header.vue
+++ components/Partials/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="relative overflow-hidden md:flex md:w-1/2 p-5 md:p-0 text-center md:text-left bg-gradient-to-tr from-blue-800 to-red-700 i justify-around items-center bg-image">
+        class="relative overflow-hidden md:flex md:w-1/2 p-5 md:p-0 text-center md:text-left bg-gradient-to-tr from-blue-800 to-red-700 i justify-around items-center bg-image py-20">
         <div>
             <h1 class="text-white font-bold text-4xl font-sans">City Government of Malolos</h1>
             <p class="text-white mt-1 hidden md:block">Government Aid Services</p>
@@ -20,5 +20,9 @@ export default({
     background-size: cover;
     background-position-x: center;
     background-position-y: center;
+}
+.py-20 {
+    padding-top: 5rem;
+    padding-bottom: 5rem;
 }
 </style>
