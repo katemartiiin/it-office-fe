173a7d536c8cba4748fd3b365e306106ec27941e
Jerick-CM <jmangaluz@gmail.com>
2022-06-30 10:38:26 +0800

    ADD

---
+++ mixins/exports/vuedatatable_mo_release.js
@@ -0,0 +1,84 @@
+export const exports_mo_release = {
+  data: () => ({
+    serverParams_mo_release: {
+      columnFilters: {},
+      sort: [
+        {
+          field: '',
+          type: '',
+        },
+      ],
+      page: 1,
+      perPage: 10,
+    },
+    columns_mo_release: [
+      {
+        label: 'Request ID.',
+        field: 'id',
+      },
+      {
+        label: 'Control No.',
+        field: 'control_number',
+      },
+      {
+        label: 'Name',
+        field: 'payee',
+      },
+      {
+        label: 'Request',
+        field: 'requestType',
+      },
+      {
+        label: 'Date / Time',
+        field: 'updated',
+      },
+      {
+        label: 'Action',
+        field: 'action',
+      },
+    ],
+    rows_mo_release: [],
+    totalRecords_mo_release: 0,
+  }),
+  methods: {
+    updateParams_mo_release(newProps) {
+      this.serverParams_mo_release = Object.assign(
+        {},
+        this.serverParams_mo_release,
+        newProps
+      )
+    },
+
+    onPageChange_mo_release(params) {
+      this.updateParams_mo_release({ page: params.currentPage })
+      this.loadItems_mo_release()
+    },
+
+    onPerPageChange_mo_release(params) {
+      this.updateParams_mo_release({ perPage: params.currentPerPage })
+      this.loadItems_mo_release()
+    },
+
+    onSortChange_mo_release(params) {
+      this.updateParams_mo_release({
+        sort: [
+          {
+            type: params[0].type,
+            field: params[0].field,
+          },
+        ],
+      })
+      this.loadItems_mo_release()
+    },
+
+    onColumnFilter_mo_release(params) {
+      this.updateParams_mo_release(params)
+      this.loadItems_mo_release()
+    },
+
+    onSearch_mo_release(params) {
+      this.updateParams_mo_release({ searchTerm: params.searchTerm })
+      this.loadItems_mo_release()
+    },
+  },
+}
---
+++ mixins/exports/vuedatatable_treasury_collection.js
@@ -0,0 +1,88 @@
+export const treasury_collection_voucher = {
+  data: () => ({
+    serverParams_treasury_collection: {
+      columnFilters: {},
+      sort: [
+        {
+          field: '',
+          type: '',
+        },
+      ],
+      page: 1,
+      perPage: 10,
+    },
+    columns_treasury_collection: [
+      {
+        label: 'Request ID.',
+        field: 'id',
+      },
+      {
+        label: 'Control No.',
+        field: 'control_number',
+      },
+      {
+        label: 'Name',
+        field: 'payee',
+      },
+      {
+        label: 'Amount',
+        field: 'particulars_amount',
+      },
+      {
+        label: 'Description',
+        field: 'particulars_description',
+      },
+      {
+        label: 'Date / Time',
+        field: 'updated',
+      },
+      {
+        label: 'Action',
+        field: 'action',
+      },
+    ],
+    rows_treasury_collection: [],
+    totalRecords_treasury_collection: 0,
+  }),
+  methods: {
+    updateParams_treasury_collection(newProps) {
+      this.serverParams_treasury_collection = Object.assign(
+        {},
+        this.serverParams_treasury_collection,
+        newProps
+      )
+    },
+
+    onPageChange_treasury_collection(params) {
+      this.updateParams_treasury_collection({ page: params.currentPage })
+      this.loadItems_treasury_collection()
+    },
+
+    onPerPageChange_treasury_collection(params) {
+      this.updateParams_treasury_collection({ perPage: params.currentPerPage })
+      this.loadItems_treasury_collection()
+    },
+
+    onSortChange_treasury_collection(params) {
+      this.updateParams_treasury_collection({
+        sort: [
+          {
+            type: params[0].type,
+            field: params[0].field,
+          },
+        ],
+      })
+      this.loadItems_treasury_collection()
+    },
+
+    onColumnFilter_treasury_collection(params) {
+      this.updateParams_treasury_collection(params)
+      this.loadItems_treasury_collection()
+    },
+
+    onSearch_treasury_collection(params) {
+      this.updateParams_treasury_collection({ searchTerm: params.searchTerm })
+      this.loadItems_treasury_collection()
+    },
+  },
+}
