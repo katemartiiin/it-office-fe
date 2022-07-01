07d64a62be38f80dd7aec15687194abc9d8513ac
Kate Martin <katejaneen23@gmail.com>
2022-07-01 13:44:27 +0800

    Changed table name

--- components/Dashboards/treasury.vue
+++ components/Dashboards/treasury.vue
@@ -483,7 +483,7 @@
         </div>
       </div>
 
-      <h2 class="text-xl font-bold py-5">Collection</h2>
+      <h2 class="text-xl font-bold py-5">Pending Collections</h2>
       <div class="rounded-t mb-0 px-4 py-5 border-0 bg-slate-600">
         <div class="flex flex-wrap items-center">
           <div class="relative w-full px-4 max-w-full flex-grow flex-1">
